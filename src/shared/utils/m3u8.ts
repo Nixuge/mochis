2195091ddce418844d71551b5eaeb7513c4fe114
Nixuge <mail@nixuge.me>
2023-12-22 19:02:59 +0100

    M3u8Opts

--- src/shared/utils/m3u8.ts
+++ src/shared/utils/m3u8.ts
@@ -2,8 +2,14 @@ import { IVideo } from '../models/types';
 // Note: this regex gets the vertical resolution only, only thing we need for the quality enum.
 const M3U8_PATTERN = /#EXT-X-STREAM-INF:[^\n]*RESOLUTION=\d+x(\d+)[^\n]*\n([^\n]+)/g; 
 
-export async function getM3u8Qualities(mainFileUrl: string, keepAuto: boolean = true, customPattern?: RegExp): Promise<IVideo[]> {
-    const usedPattern = customPattern ? customPattern : M3U8_PATTERN;
+export type M3u8Opts = {
+    keepAuto?: boolean,
+    customPattern?: RegExp,
+    headers?: Record<string, string>
+}
+
+export async function getM3u8Qualities(mainFileUrl: string, opts: M3u8Opts = {}): Promise<IVideo[]> {
+    const usedPattern = opts.customPattern ? opts.customPattern : M3U8_PATTERN;
 
     const urlSplit = mainFileUrl.split("/")
     const baseDomain = urlSplit.slice(0, 3).join("/") + "/"
@@ -11,14 +17,14 @@ export async function getM3u8Qualities(mainFileUrl: string, keepAuto: boolean = 
     const baseUrl =  urlSplit.join("/") + "/"
 
     const sources: IVideo[] = []
-    if (keepAuto)
+    if (opts.keepAuto === undefined || opts.keepAuto)
         sources.push({
             url: mainFileUrl,
             quality: "auto",
             isDASH: false,
           } satisfies IVideo)
     
-    const m3u8Data = await request.get(mainFileUrl).then(resp => resp.text());
+    const m3u8Data = await request.get(mainFileUrl, { headers: opts.headers }).then(resp => resp.text());
     const m3u8Sources = m3u8Data.matchAll(usedPattern);
     for (const m3u8Source of m3u8Sources) {
         const quality = m3u8Source[1];
