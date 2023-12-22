807a402b68f9ee5b7251721c5f8cbed48efb80ba
Nixuge <mail@nixuge.me>
2023-12-22 17:30:43 +0100

    animesama's extractors using getSource

--- src/animesama/extractors/index.ts
+++ src/animesama/extractors/index.ts
@@ -1,13 +1,13 @@
 import { VkE } from "./vk"
 import { SibnetE } from "./sibnet";
 import { SendVidE } from "./sendvid";
-import { IVideo } from "../../shared/models/types";
+import { ISource, IVideo } from "../../shared/models/types";
 
-export async function getVideo(url: string): Promise<IVideo[]> {
+export async function getVideo(url: string): Promise<ISource> {
     console.log(url);
     
     if (url.includes(".anime-sama.fr/videos/")) {
-        return [{ url: url, isDASH: true }]
+        return { sources: [{ url: url, isDASH: true }] }
     }
 
     const html = (await request.get(url)).text();
@@ -16,10 +16,10 @@ export async function getVideo(url: string): Promise<IVideo[]> {
         return new SibnetE(url, html).extract()
     }
     if (url.startsWith("https://vk.com/video_ext.php?")) {
-        return new VkE(url, html).extract()
+        return new VkE(url, html).getSource();
     }
     if (url.startsWith("https://sendvid.com/embed/")) {
-        return new SendVidE(url, html).extract()
+        return new SendVidE(url, html).getSource()
     }
     
     throw Error("No extractor for url " + url);
