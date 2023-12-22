ded047f62aff38932e0c3708ee1416ba4622d7eb
Nixuge <mail@nixuge.me>
2023-12-22 17:30:25 +0100

    sendvid source

--- src/animesama/extractors/sendvid.ts
+++ src/animesama/extractors/sendvid.ts
@@ -1,11 +1,13 @@
 import { VideoExtractor } from '../../shared/models/Iextractor';
 import { IVideo } from '../../shared/models/types';
+import { getM3u8Qualities } from '../../shared/utils/m3u8';
 
 export class SendVidE extends VideoExtractor {
   protected override serverName = 'sendvid';
   protected override sources: IVideo[] = [];
 
   override extract = async (): Promise<IVideo[]> => {
-    throw Error("Not yet implemented")
+    const url = this.htmlContent.match(/var video_source = "(.*?)";/)![1];
+    return await getM3u8Qualities(url);
   }
 }
