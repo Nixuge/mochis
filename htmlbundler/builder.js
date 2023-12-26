2e7320939dc9d8027445b6d5eaa91a9ec2bfd05c
Nixuge <mail@nixuge.me>
2023-12-26 12:47:29 +0100

    Made htmlbundler cleaner

--- htmlbundler/builder.js
+++ htmlbundler/builder.js
@@ -1,36 +1,36 @@
 import { readFileSync, writeFileSync } from "fs";
 
-let start = readFileSync('./htmlbundler/part1.html').toString();
-const end = readFileSync('./htmlbundler/part2.html').toString();
+let html = readFileSync('./htmlbundler/base.html').toString();
 
 const repo = JSON.parse(readFileSync('./dist/Manifest.json'));
 const name = repo.repository.name;
 
-start = start.replaceAll("!!!TITLE!!!", name);
+html = html.replaceAll("!!!TITLE!!!", name);
 
-let htmlString = start;
+let modulesHtml = "";
 for (const module of repo.modules) {
-    htmlString += `            <li class="fade-in">
-    <div class="module">
-        <div class="image-wrap">
-            <img width="40" height="40" src="${module.icon}">
-        </div>
-        <div class="module-meta">
-            <h2 class="module-title">${module.name}</h2>
-            <span class="module-version">v${module.version}</span>
-        </div>
-        <div class="install-module-wrap">
-        <a class="install-module-button" onclick="addModule('${module.file}')">Add</a>
-    </div>
-    </div>
-    <hr>
-</li>`
+    modulesHtml += `
+            <li class="fade-in">
+                <div class="module">
+                    <div class="image-wrap">
+                        <img width="40" height="40" src="${module.icon}">
+                    </div>
+                    <div class="module-meta">
+                        <h2 class="module-title">${module.name}</h2>
+                        <span class="module-version">v${module.version}</span>
+                    </div>
+                    <div class="install-module-wrap">
+                        <a class="install-module-button" onclick="addModule('${module.file}')">Add</a>
+                    </div>
+                </div>
+                <hr>
+            </li>`
 }
 
-htmlString += end;
+html = html.replace("!!!MODULES!!!", modulesHtml)
 
 try {
-    writeFileSync("./dist/index.html", htmlString, 'utf8');
+    writeFileSync("./dist/index.html", html, 'utf8');
     console.log('✔ Successfully wrote new html to disk');
   } catch (error) {
     console.log('⚠ An error has occurred writing new html ', error);
