684f78a4f962514aa306a8199e4c2cf98b0e4c32
Nixuge <mail@nixuge.me>
2024-01-02 20:03:20 +0100

    netu cleanup + variable id + comments for another bypass

--- src/shared/extractors/netu.ts
+++ src/shared/extractors/netu.ts
@@ -39,10 +39,22 @@ function deobfLink(target: string) {
   if (target.indexOf('.') == -1) {
     target = target.substr(1);
     let s2 = '';
-    for (let i=0; i < target.length; i += 3) s2 += '%u0' + target.slice(i, i + 3);
+    for (let i = 0; i < target.length; i += 3) s2 += '%u0' + target.slice(i, i + 3);
     target = unescape(s2)
+  }
+  return target
 }
-return target
+function makeid() {
+  return makeid_var(32); //default netu value, from embed.js
+}
+function makeid_var(length: number) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_*';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
 export class NetuE extends VideoExtractor {
@@ -56,9 +68,10 @@ export class NetuE extends VideoExtractor {
     let v = iframe_url.replace("/e/", "").split("?")[0];
     iframe_url = hostname + iframe_url;
 
-    const iframe_text = await request.get(iframe_url).then(resp => resp.text())
-    const adb = iframe_text.match(/adbn = \'(.*?)\',$/m)![1]
-    const [_, video_id, video_key] = iframe_text.match(/'videoid': '([0-9]*)',.*?'videokey': '(.*?)'/s)!
+    const iframe_text = await request.get(iframe_url).then(resp => resp.text());
+    const adb = iframe_text.match(/adbn = \'(.*?)\',$/m)![1];
+
+    const [_, video_id, video_key] = iframe_text.match(/'videoid': '([0-9]*)',.*?'videokey': '(.*?)'/s)!;
     const image_payload = {
         "videoid": video_id,
         "videokey": video_key,
@@ -73,7 +86,7 @@ export class NetuE extends VideoExtractor {
             "X-Requested-With": "XMLHttpRequest",
             "Origin": hostname
         }
-    }).then(resp => resp.json())
+    }).then(resp => resp.json());
     
     if (!imageGetSuccess(image_res)) {
       throw Error("Getting image failed! isec: " + image_res.isec)
@@ -83,12 +96,20 @@ export class NetuE extends VideoExtractor {
       body: JSON.stringify({"b64image": image_res.image}),
       headers: {"Content-Type": "application/json"}
     }
-    ).then(req => req.json())
+    ).then(req => req.json());
     
     const [x, y, radius] = playButtonCoords;
+
+    // Note:
+    // Hardcoded for now as this doesn't seem to change.
+    // This is located inside the embed.js, in the super long line with chinese characters.
+    // If this becomes variable, here's a way to get it:
+    // const shh = eval(`function owo(){${CHINESE_LINE_HERE}; return shh;} owo()`)
+    // This will require another request tho, which is why i'm not doing it rn.
+    const shh = "762384534ad2168058529199987f15351806dc76";
     
     const body = {
-      'sh':"762384534ad2168058529199987f15351806dc76", // HARDCODED FOR NOW. NEED TO EVAL THE HUGE CHINESE TEXT THEN RETURN "shh"
+      'sh': shh, 
       'ver':'4',
       'secure':"0",
       'adb': adb,
@@ -101,10 +122,10 @@ export class NetuE extends VideoExtractor {
       'click_hash':encodeURIComponent(image_res.hash_image),
       'clickx':x,
       'clicky':y
-    }
+    };
     const headers = {
       "content-type": "application/json",
-      "cookie": "EU_COOKIE_LAW_CONSENT=true; uid=9SxCjxsrmotiLvDk*MFi6E9p8xMcRasF",
+      "cookie": `EU_COOKIE_LAW_CONSENT=true; uid=${makeid()}`,
       "origin": "https://waaw.to",
       "pragma": "no-cache",
       "referer": iframe_url,
@@ -116,14 +137,14 @@ export class NetuE extends VideoExtractor {
       "sec-fetch-site": "same-origin",
       "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
       "x-requested-with": "XMLHttpRequest"
-    }
+    };
     console.log(JSON.stringify(body));
     console.log(JSON.stringify(headers));
     
     
     const finalResp: DataResponse = await request.post(hostname + "/player/get_md5.php", {
       body: JSON.stringify(body), headers
-    }).then(resp => resp.json())    
+    }).then(resp => resp.json());
 
     const link = finalResp.obf_link;
     if (!link || link == "#") {
@@ -132,11 +153,11 @@ export class NetuE extends VideoExtractor {
       throw Error("Failed to grab link!" + JSON.stringify(finalResp));
     }
 
-    let clearLink = deobfLink(link)
+    let clearLink = deobfLink(link);
     if (clearLink.startsWith("//"))
-      clearLink = "https:" + clearLink
+      clearLink = "https:" + clearLink;
     else if (clearLink.startsWith("/"))
-      clearLink = hostname + clearLink
+      clearLink = hostname + clearLink;
 
     console.log("SUCCESS GRABBING NETU LINK! " + clearLink);
 
