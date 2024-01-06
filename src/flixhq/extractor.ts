7e72085603c04651ed712c3f85f8ca876a730a32
Nixuge <mail@nixuge.me>
2024-01-06 23:43:07 +0100

    Try 0.1.19

--- src/flixhq/extractor.ts
+++ src/flixhq/extractor.ts
@@ -1,25 +1,22 @@
 // import { DoodE } from '../shared/extractors/dood';
-// import { UpstreamE } from '../shared/extractors/upstream';
-import { VidCloudE } from '../shared/extractors/vidcloud';
+import { UpstreamE } from '../shared/extractors/upstream';
+// import { VidCloudE } from '../shared/extractors/vidcloud';
 // import { VoeE } from '../shared/extractors/voe';
 import { ISource } from '../shared/models/types';
 
 
 export async function getVideo(url: string, provider: string): Promise<ISource> {
-    provider = provider.toLowerCase()
+    provider = provider.toLowerCase()    
 
-    console.warn("If you see this, it's a good sign. v0.1.17");
-    
+    if (provider == "mixdrop")
+        throw Error("Mixdrop is down");
 
-    // if (provider == "mixdrop")
-    //     throw Error("Mixdrop is down");
-
-    if (provider == "upcloud" || provider == "vidcloud")
-        return await new VidCloudE(url).extract();
+    // if (provider == "upcloud" || provider == "vidcloud")
+    //     return await new VidCloudE(url).extract();
         
-    // const html = await request.get(url).then(resp => resp.text())
-    // if (provider == "upstream")
-    //     return await new UpstreamE(url, html).getSource();
+    const html = await request.get(url).then(resp => resp.text())
+    if (provider == "upstream")
+        return await new UpstreamE(url, html).getSource();
 
     // if (provider == "doodstream")
     //     return await new DoodE(url, html).getSource();
