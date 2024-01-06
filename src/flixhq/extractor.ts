2220bcc077a95d7df5f353d9fca644bfc4ec0575
Nixuge <mail@nixuge.me>
2024-01-06 20:33:14 +0100

    Added Dood, Upstream & Voe to flixhq

--- src/flixhq/extractor.ts
+++ src/flixhq/extractor.ts
@@ -1,14 +1,27 @@
+import { DoodE } from '../shared/extractors/dood';
+import { UpstreamE } from '../shared/extractors/upstream';
 import { VidCloudE } from '../shared/extractors/vidcloud';
+import { VoeE } from '../shared/extractors/voe';
 import { ISource } from '../shared/models/types';
 
 export async function getVideo(url: string, provider: string): Promise<ISource> {
     provider = provider.toLowerCase()
 
     if (provider == "mixdrop")
-        throw Error("Mixdrop is down")
+        throw Error("Mixdrop is down");
 
     if (provider == "upcloud" || provider == "vidcloud")
-        return await new VidCloudE(url).extract()
+        return await new VidCloudE(url).extract();
+        
+    const html = await request.get(url).then(resp => resp.text())
+    if (provider == "upstream")
+        return await new UpstreamE(url, html).getSource();
+
+    if (provider == "doodstream")
+        return await new DoodE(url, html).getSource();
+
+    if (provider == "voe")
+        return await new VoeE(url, html).extract();
     
     throw Error("No extractor for url " + url + "(provider: " + provider + ")");
 }
--- tests/flixhq.test.ts
+++ tests/flixhq.test.ts
@@ -3,7 +3,7 @@ import runner from "@mochiapp/runner";
 import Source from "../src/flixhq";
 
 import { log, logEnabled } from "./utils/log"
-logEnabled(false)
+logEnabled(true)
 
 const source = runner(Source)
 
@@ -17,9 +17,9 @@ const source = runner(Source)
 
 
 
-test("provides correct search info", async () => {
-  log(await source.search({ query: "word", filters: [] }))
-})
+// test("provides correct search info", async () => {
+//   log(await source.search({ query: "word", filters: [] }))
+// })
 
 
 // test("provides playlist details", async () => {
@@ -34,25 +34,27 @@ let episodeId: string = ""
 //   // episodeId = data[0].variants?.[0].pagings?.[0].items?.[0].id!
 // })
 // test("gets the seasons", async() => {
-//   const data = await source.playlistEpisodes("/tv/watch-hunter-x-hunter-39509");
-//   log(JSON.stringify(data))
-//   // episodeId = data[0].variants?.[0].pagings?.[0].items?.[0].id!
+//   const data = await source.playlistEpisodes("/tv/watch-hunter-x-hunter-36456");
+//   // log(JSON.stringify(data))
+//   episodeId = data[0].variants?.[0].pagings?.[0]
+//   log(episodeId)
 // })
-// test("gets the episodes for season 7", async() => {
-//   const data = await source.playlistEpisodes("/tv/watch-hunter-x-hunter-39509", {type: 'group', groupId: "358"});
-//   log(JSON.stringify(data))
-//   // episodeId = data[0].variants?.[0].pagings?.[0].items?.[0].id!
-// })
+test("gets the episodes for season 7", async() => {
+  const data = await source.playlistEpisodes("/tv/watch-hunter-x-hunter-36456", {type: 'group', groupId: "16717"});
+  log(JSON.stringify(data))
+  episodeId = data[0].variants?.[0].pagings?.[0].items?.[0].id!
+  log(episodeId)
+})
 
 
-// test("gets the episode sources", async() => {
-//   log(JSON.stringify(await source.playlistEpisodeSources({playlistId: "", episodeId: "/ajax/episode/servers/6366"})))
-// })
+test("gets the episode sources", async() => {
+  log(JSON.stringify(await source.playlistEpisodeSources({playlistId: "", episodeId: episodeId})))
+})
 
-// test("gets an episode server", async() => {
-//   log(JSON.stringify(await source.playlistEpisodeServer({
-//     playlistId: "/tv/watch-hunter-x-hunter-39509", 
-//     episodeId: "", 
-//     sourceId:"", 
-//     serverId:"{\"id\":\"4857184\",\"provider\":\"UpCloud\"}"})))
-// })
+test("gets an episode server", async() => {
+  log(JSON.stringify(await source.playlistEpisodeServer({
+    playlistId: "/tv/watch-hunter-x-hunter-36456", 
+    episodeId: "", 
+    sourceId:"", 
+    serverId:"{\"id\":\"7489777\",\"provider\":\"Voe\"}"})))
+})
