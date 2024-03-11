63e5a11a95f827fb94bb4523fd7a7e812a4937e9
Nixuge <mail@nixuge.me>
2024-03-11 20:28:03 +0100

    Fixed filemoon changing url

--- src/aniwave/extractor.ts
+++ src/aniwave/extractor.ts
@@ -7,7 +7,7 @@ export async function getVideo(url: string): Promise<ISource> {
     const html = (await request.get(url, {headers: {"Referer": "https://aniwave.to/"}})).text();
     
     // Dirty for now
-    if (url.startsWith("https://filemoon.sx/")) {
+    if (url.startsWith("https://filemoon.sx/") || url.startsWith("https://kerapoxy.cc/") || url.startsWith("https://hellnaw.cc/")) {
         return new FilemoonE(url, html).extract()
     }
     if (url.startsWith("https://www.mp4upload.com/")) {
