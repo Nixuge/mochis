8e0f3c6840b1159669d21ccddf25e928fcc8532e
Nixuge <mail@nixuge.me>
2024-03-20 15:56:45 +0100

    Added subtitles

--- src/shared/extractors/vidcloud.ts
+++ src/shared/extractors/vidcloud.ts
@@ -1,11 +1,8 @@
-import CryptoJS from 'crypto-js';
 import { RawVideoExtractor } from '../models/Iextractor';
 import { ISource } from '../models/types';
 import { getM3u8Qualities } from '../utils/m3u8';
-import { IRawTrackMedia, parseSubtitles } from '../utils/subtitles';
+import { PlaylistEpisodeServerSubtitle, PlaylistEpisodeServerSubtitleFormat } from '@mochiapp/js/dist';
 
-// Mostly from consumet,
-// thanks to them & theonlymo
 
 type ResponseType = {
     source: string,
@@ -22,17 +19,33 @@ export class VidCloudE extends RawVideoExtractor {
         console.log("Note: this is a beta. I have no idea if it's going to hold when a lot of users use it.");
         console.log("If you see this in the logs followed by an error for the request, consider it normal.");
         
-        const data: ResponseType = await request.post("https://rabbitthunder-test.vercel.app/api/upcloud", {
+        const data: ResponseType = await request.post("https://rabbitthunder-test.vercel.app/api/vidcloud", {
           headers: {"Content-Type": "application/json"},
           body: JSON.stringify({"id": id})}
         ).then(req => req.json())
 
 
         const videos = await getM3u8Qualities(data.source);
-        
+        const subtitles: PlaylistEpisodeServerSubtitle[] = [];
+        for (const subtitle of data.subtitle) {
+          let name = subtitle;
+          try {
+            const splitted = subtitle.split("/");
+            name = splitted[splitted.length-1].split(".vtt")[0]
+          } catch {}
+          subtitles.push({
+            url: subtitle,
+            name,
+            format: PlaylistEpisodeServerSubtitleFormat.vtt,
+            default: false,
+            autoselect: false
+          } satisfies PlaylistEpisodeServerSubtitle)
+        }
+
+
         return {
             videos,
-            // subtitles: parseSubtitles(data.subtitle)
+            subtitles
         };
     };
 }
