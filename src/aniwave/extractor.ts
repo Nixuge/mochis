7de183d533a5efac9cd2bcc83799b6159f4da6f4
Nixuge <mail@nixuge.me>
2024-03-30 02:33:42 +0100

    Aniwave update: use MyCloud extractor by default.

--- src/aniwave/extractor.ts
+++ src/aniwave/extractor.ts
@@ -16,6 +16,11 @@ export async function getVideo(url: string): Promise<ISource> {
     if (url.startsWith("https://mcloud.bz/") || url.startsWith("https://vidplay.online/") || url.startsWith("https://ea1928580f.site/")) {
         return new MyCloudE(url, html).extract()
     }
-    
-    throw Error("No extractor for url " + url);
+
+    console.warn(`No extractor found for url ${url}. 
+    HOWEVER, due to the fact vidplay really enjoy changing their domain often, the vidplay extractor is going to be used.
+    I could rewrite some parts of the module to simply pass along the name of the server instead of the url only, but too much work ngl (5mins).
+    (+ w how the system is made it's technically made to only pass 1 property which is the url here)`);
+
+    return new MyCloudE(url, html).extract()
 }
