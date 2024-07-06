60258c15677bcb85632da93da6573eff59d32180
Nixuge <mail@nixuge.me>
2024-07-07 01:51:20 +0200

    Aniwave tests now have proper urls in the same format as in the app

--- tests/aniwave.test.ts
+++ tests/aniwave.test.ts
@@ -3,7 +3,7 @@ import runner from "@mochiapp/runner";
 import Source from "../src/aniwave";
 
 import { log, logEnabled } from "./utils/log"
-// logEnabled(true)
+logEnabled(true)
 
 const source = runner(Source)
 
@@ -20,24 +20,24 @@ const source = runner(Source)
 // })
 
 // test("provides playlist details", async () => {
-//   console.log(await source.playlistDetails("/watch/pokemon.r9vq/ep-1"))
+//   console.log(await source.playlistDetails("/watch/pokemon.r9vq"))
 // })
 
+test("gets the episode count", async() => {
+  log((await source.playlistEpisodes("/watch/pokemon.r9vq")))
+})
 // test("gets the episode count", async() => {
-//   log(JSON.stringify(await source.playlistEpisodes("/watch/pokemon.r9vq/ep-1")))
-// })
-// test("gets the episode count", async() => {
-//   JSON.stringify(await source.playlistEpisodes("/watch/boushoku-no-berserk-ore-dake-reberu-to-iu-gainen-wo-toppa-suru.ojll4/ep-1"))
+//   JSON.stringify(await source.playlistEpisodes("/watch/boushoku-no-berserk-ore-dake-reberu-to-iu-gainen-wo-toppa-suru.ojll4"))
 // })
 // test("gets the episode count", async() => {
 //   log(JSON.stringify(await source.playlistEpisodes("/watch/hunter-x-hunter-2011.295")))
 // })
 
 // test("gets the episode sources", async() => {
-//   log(JSON.stringify(await source.playlistEpisodeSources({playlistId: "how-not-to-summon-a-demon-lord-w.oz6y/ep-1", episodeId: "HTmfCcgl,HTSZA8sl,HTqXDM4m | softsub"})))
+//   log(JSON.stringify(await source.playlistEpisodeSources({playlistId: "/watch/how-not-to-summon-a-demon-lord-w.oz6y", episodeId: "HTmfCcgl,HTSZA8sl,HTqXDM4m | softsub"})))
 // })
 
 // timeout 15s as the deobfuscation can take some time when the js changes.
 // test("gets an episode server", async() => {
-//   log(JSON.stringify(await source.playlistEpisodeServer({playlistId: "how-not-to-summon-a-demon-lord-w.oz6y/ep-1", episodeId: "HTmfCcgl,HTSZA8sl,HTqXDM4m", sourceId:"servers", serverId:"GDyXC84gkg=="})))
+//   log(JSON.stringify(await source.playlistEpisodeServer({playlistId: "/watch/how-not-to-summon-a-demon-lord-w.oz6y", episodeId: "HTmfCcgl,HTSZA8sl,HTqXDM4m", sourceId:"servers", serverId:"GDyXC84gkg=="})))
 // }, {timeout: 20000})
