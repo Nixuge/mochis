50d28ceb78e960baadf8c9aa0ece2a8a9fef22a9
Nixuge <mail@nixuge.me>
2024-07-07 12:22:26 +0200

    mycloud key based extractor for faster loading (with chromium fallback)

--- src/shared/extractors/mycloud.ts
+++ src/shared/extractors/mycloud.ts
@@ -1,8 +1,9 @@
-import { PlaylistEpisodeServerSubtitleFormat } from '@mochiapp/js/dist';
 import { VideoExtractor } from '../models/Iextractor';
-import { ISource, IVideo } from '../models/types';
+import { ISource } from '../models/types';
 import { getM3u8Qualities } from '../utils/m3u8';
 import { IRawTrackMedia, parseSubtitles } from '../utils/subtitles';
+import { rc4Cypher } from '../utils/aniwave/rc4';
+import { b64encode } from '../utils/aniwave/b64encode';
 
 interface IMediaInfo {
   status: number,
@@ -11,6 +12,84 @@ interface IMediaInfo {
     tracks: IRawTrackMedia[]
   }
 }
+
+// KEYS DECRYPTION //
+async function grabKeysFromGithub(e) {
+  const resp = await request.get(e).then(s => s.text())!
+  return resp.match(/"rawLines":\["\[\\"(.+)\\"]"\]/)![1].split('\\",\\"')
+}
+
+async function getToken(domain: string, useCached: boolean) {
+  // Note: it doesn't seem like grabbing a new token is necessary for some reason?
+  if (useCached)
+    return 'ViFRsqNPsIHKpYB0WLBjGjDGLa4flllPaeQmJ2GWwnXjR6wupwiKOdg92mKTSXrGkg==';
+  
+  const futokenText = await request.get(`https://vidplay.online/futoken`, {
+      headers: {
+          referer: domain
+      }
+  }).then(u => u.text())
+  return futokenText.match(/var\s+k\s*=\s*'([^']+)'/)?.[1] ?? "";
+}
+
+function futoken(v: string, location: string, domain: string, token: string): string {
+  const a: (string | number)[] = [token];
+  
+  for (let i = 0; i < v.length; i++) {
+      a.push(token.charCodeAt(i % token.length) + v.charCodeAt(i));
+  }
+
+  return `https://${domain}/mediainfo/${a.join(',')}${location}`;
+}
+
+async function getUrl(fullUrl: string, keyUrl: string, cachedFutoken: boolean) {
+  let videoId = fullUrl.split("/e/")[1].split("?")[0];
+  let keys = await grabKeysFromGithub(keyUrl);
+
+  const firstPass = rc4Cypher(keys[0], videoId);
+  const secondPass = rc4Cypher(keys[1], firstPass);
+  const secondPassEncoded = b64encode(secondPass).replace("/", "_");
+
+  const urlEnd = "?" + fullUrl.split("?").pop();
+  const domain = fullUrl.split('/')[2];
+
+  const token = await getToken(fullUrl, cachedFutoken);
+
+  return futoken(secondPassEncoded, urlEnd, domain, token)
+}
+
+async function attemptDecodingKeys(url: string, cachedToken: boolean) {
+  const keyUrls = ["https://github.com/KillerDogeEmpire/vidplay-keys/blob/keys/keys.json", "https://github.com/Ciarands/vidsrc-keys/blob/main/keys.json"];
+  for (let keyUrl of keyUrls) {
+      const sourcesUrl = await getUrl(`${url}&autostart=true`, keyUrl, cachedToken);
+
+      let sourcesRes: IMediaInfo = await request.get(sourcesUrl, {
+              headers: {
+                  Referer: url
+              }
+          }).then(f => f.json());
+      
+      // @ts-ignore
+      // Basically, when the url is invalid, result is set to "404" instead of the normal object
+      // with sources & tracks.
+      if (sourcesRes.result != 404) {
+          return sourcesRes;
+      }
+  }
+  throw Error("Couldn't get source.")
+}
+// KEYS DECRYPTION END //
+
+// SELENIUM DECRYPTION //
+async function attemptDecodingSelenium(url: string) {
+  const mediaInfo: IMediaInfo = await request.post("https://anithunder.vercel.app/api/mcloud", {
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({"url": url})}
+  ).then(req => req.json())
+
+  return mediaInfo;
+}
+// SELENIUM DECRYPTION END //
 
 // Note: this is named mycloud but works for both mycloud & vidplay
 export class MyCloudE extends VideoExtractor {
@@ -23,12 +102,22 @@ export class MyCloudE extends VideoExtractor {
     // Server side is handling more things than previously.
     // If I want to reverse to how it was done before, check commit before (including) this one:
     // https://github.com/Nixuge/mochis/commit/ce615f9ff486ec82b01dcdcb8e6d08a987871d8d
-    
-    const mediaInfo: IMediaInfo = await request.post("https://anithunder.vercel.app/api/mcloud", {
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify({"url": url})}
-    ).then(req => req.json())
-    
+
+    let mediaInfo: IMediaInfo;
+    try {
+      console.log("attempting extraction using a cached token");
+      mediaInfo = await attemptDecodingKeys(url, true);
+    } catch(e) {
+      try {
+        console.log("attempting extraction using a fresh token");
+        mediaInfo = await attemptDecodingKeys(url, false);
+      } catch(e) {
+        console.log("attempting extraction using a browser");
+        mediaInfo = await attemptDecodingSelenium(url);
+        // If this fails again, just let it throw an error...
+      }
+    }
+
     const sourcesJson = mediaInfo.result.sources;
     
     const videos = await getM3u8Qualities(sourcesJson[0]["file"])
--- tests/aniwave.test.ts
+++ tests/aniwave.test.ts
@@ -29,15 +29,15 @@ const source = runner(Source)
 // test("gets the episode count", async() => {
 //   JSON.stringify(await source.playlistEpisodes("/watch/boushoku-no-berserk-ore-dake-reberu-to-iu-gainen-wo-toppa-suru.ojll4"))
 // })
-test("gets the episode count", async() => {
-  log(JSON.stringify(await source.playlistEpisodes("/watch/hunter-x-hunter-2011.295")))
-})
+// test("gets the episode count", async() => {
+//   log(JSON.stringify(await source.playlistEpisodes("/watch/hunter-x-hunter-2011.295")))
+// })
 
 // test("gets the episode sources", async() => {
 //   log(JSON.stringify(await source.playlistEpisodeSources({playlistId: "/watch/how-not-to-summon-a-demon-lord-w.oz6y", episodeId: "HTmfCcgl,HTSZA8sl,HTqXDM4m | softsub"})))
 // })
 
 // timeout 15s as the deobfuscation can take some time when the js changes.
-// test("gets an episode server", async() => {
-//   log(JSON.stringify(await source.playlistEpisodeServer({playlistId: "/watch/how-not-to-summon-a-demon-lord-w.oz6y", episodeId: "HTmfCcgl,HTSZA8sl,HTqXDM4m", sourceId:"servers", serverId:"GDyXC84gkg=="})))
-// }, {timeout: 20000})
+test("gets an episode server", async() => {
+  log(JSON.stringify(await source.playlistEpisodeServer({playlistId: "/watch/how-not-to-summon-a-demon-lord-w.oz6y", episodeId: "HTmfCcgl,HTSZA8sl,HTqXDM4m", sourceId:"servers", serverId:"GDyXC84gkg=="})))
+}, {timeout: 20000})
