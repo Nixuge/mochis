042a97cd811f553dedd2085c21e4da23821b9b9e
Nixuge <mail@nixuge.me>
2024-07-07 23:23:53 +0200

    Added vidoza extractor

---
+++ src/shared/extractors/vidoza.ts
@@ -0,0 +1,23 @@
+import { VideoExtractor } from '../../shared/models/Iextractor';
+import { ISource, IVideo } from '../../shared/models/types';
+
+export class VidozaE extends VideoExtractor {
+  protected override serverName = 'vidoza';
+
+  override extract = async (): Promise<ISource> => {
+    const html = this.htmlContent;
+
+    // Can grab either from the html or from the script.
+    // Doing from the html bc why not
+    const url = html.match(/<source src=\"(.*?)\".*?>/s)![1]
+
+    // Don't think this source has subtitles.
+
+    return {
+      videos: [{
+        url: url,
+        isDASH: true
+      }]
+    }
+  };
+}
