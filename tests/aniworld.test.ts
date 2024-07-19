7afa41b6c332998272f8c733ad5748a372a589d7
Nixuge <mail@nixuge.me>
2024-07-19 20:00:52 +0200

    Fixed aniworld test for new changes

--- tests/aniworld.test.ts
+++ tests/aniworld.test.ts
@@ -11,17 +11,17 @@ const source = runner(Source)
 //   log((await source.discoverListings())[0]["paging"])
 // })
 
-// test("gets the episode count", async() => {
-//   log((await source.playlistEpisodes("quality-assurance-in-another-world"))[0]["variants"]![0]["pagings"]![0]["items"]);
-// })
+test("gets the episode count", async() => {
+  log((await source.playlistEpisodes("/anime/stream/quality-assurance-in-another-world"))[0]["variants"]![0]["pagings"]![0]["items"]);
+})
 
 test("gets the episode sources", async() => {
-  log(JSON.stringify(await source.playlistEpisodeSources({playlistId: "quality-assurance-in-another-world", episodeId: "staffel-1/episode-1/japanese-german"})))
+  log(JSON.stringify(await source.playlistEpisodeSources({playlistId: "/anime/stream/quality-assurance-in-another-world", episodeId: "staffel-1/episode-1/japanese-german"})))
 })
 
 test("gets an episode server (Movie)", async() => {
   log(JSON.stringify(await source.playlistEpisodeServer({
-    playlistId: "quality-assurance-in-another-world", 
+    playlistId: "/anime/stream/quality-assurance-in-another-world", 
     episodeId: "staffel-1/episode-1/japanese-german",
     sourceId: "aniworld",
     serverId: "doodstream/2593611"
